d584920880c549b1f34129c2dd75125d2964aae5
nikalexey <nikalexey@gmail.com>
2019-01-17 10:54:21 +0300

    update cmakelists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <signal.h>
+#include "CFile.h"
 
-int main() {
-  std::cout << "hxview" << std::endl;
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    std::cout << "using: hwview <file>" << std::endl;
+    return 1;
+  }
+  std::cout << argv[1] << std::endl;
+
+  CFile fl(argv[1]);
+  size_t size = fl.getSize();
+  std::cout << size << std::endl;
+
   return 0;
 }
