16b58ecf72f571271b1c4da1fde68892765b0b8e
nikalexey <nikalexey@gmail.com>
2019-01-17 10:52:06 +0300

    add class file

---
+++ src/CFile.cpp
@@ -0,0 +1,21 @@
+//
+// Created by anikiforov on 17.01.19.
+//
+
+#include "CFile.h"
+
+
+CFile::CFile(const std::string& filename) : m_filename(filename) {
+//todo: realize
+}
+
+CFile::~CFile() {
+//todo: realize
+
+}
+
+size_t CFile::getSize() {
+//todo: realize
+
+  return 0;
+}
